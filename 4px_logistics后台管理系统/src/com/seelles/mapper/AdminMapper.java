48a33f7864660281982c86398a82182654a07f39
依林 <oncwnuABZKw0QStENMPZi7VdtScA@git.weixin.qq.com>
2020-07-05 21:06:06 +0800

    移除多余文件

--- 4px_logistics后台管理系统/src/com/seelles/mapper/AdminMapper.java
+++
@@ -1,8 +0,0 @@
-package com.seelles.mapper;
-
-import com.seelles.pojo.Admin;
-import com.seelles.pojo.User;
-
-public interface AdminMapper {
-    public Admin selectOne(String adname, String pwd);
-}
